74dfc73210cc400f76d6350e8b020564d35b1eb3
Jakson Sanders <jsprof452@gmail.com>
2025-04-05 07:35:29 +0000

    Remove defaults and add date

--- web-dashboard/src/App.js
+++ web-dashboard/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  /*const day = new Intl.DateTimeFormat('en-US', {
+    weekday: 'long',
+  }).format(new Date())*/
+
+  const day = new Date().getDay();
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+  const date = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  }).format(new Date())
+
+  const time = new Intl.DateTimeFormat('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+  }).format(new Date())
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          {time}
+        </h1>
+        <p>{days[day]}, {date}</p>
+
       </header>
     </div>
   );
